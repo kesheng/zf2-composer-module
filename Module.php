b932a8058327974f00a48e2569da09744f0b9656
Richard He <shenghua.he@gmail.com>
2013-12-30 15:08:16 +0000

    Added the classmap section instead.

--- Module.php
+++ Module.php
@@ -1,3 +1,37 @@
 <?php
 
-require_once(__DIR___ . "/src/Zf2ComposerModule/Module.php");
+namespace Zf2ComposerModule;
+
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        $services = $e->getApplication()->getServiceManager();
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+        );
+    }
+
+}
